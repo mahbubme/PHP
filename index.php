1cc40208d0dae84ad2efe9edb79b8cb399ca3c73
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-16 17:11:46 +0000

    indentation

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php 
-phpinfo();
+$name = 'Alex';
+$age = '16';
+if (strtolower($name)==='alex') {
+    if ($age>=21) {
+        echo 'You\'re over 21';
+        if (1===1) {
+            echo 'Yes, 1 is equal to 1!';
+        }
+    } else{
+        echo 'Not over 21.';
+    }
+} else {
+    echo 'You\'re not Alex!';
+}
 ?>
