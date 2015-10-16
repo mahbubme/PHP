179971007385a5926bd40b3010d7a8c6c2953ace
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-16 17:23:54 +0000

    embedding php inside html

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php 
-$name = 'Alex';
-$age = '16';
-if (strtolower($name)==='alex') {
-    if ($age>=21) {
-        echo 'You\'re over 21';
-        if (1===1) {
-            echo 'Yes, 1 is equal to 1!';
-        }
-    } else{
-        echo 'Not over 21.';
-    }
-} else {
-    echo 'You\'re not Alex!';
-}
+$text = 'Hello World.';
 ?>
+
+<input type="text" name="name" value="<?php echo $text; ?>">
