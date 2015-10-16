6ce0a5e2ee5562e33cc64600a37effbe783e8283
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-16 17:18:39 +0000

    echo html inside php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php 
-$name = 'Alex';
-$age = '16';
-if (strtolower($name)==='alex') {
-    if ($age>=21) {
-        echo 'You\'re over 21';
-        if (1===1) {
-            echo 'Yes, 1 is equal to 1!';
-        }
-    } else{
-        echo 'Not over 21.';
-    }
-} else {
-    echo 'You\'re not Alex!';
-}
+    echo '<input type="text" name="name" value="Alex">';
 ?>
+
+<?php 
+    echo "<input type='text' name='name' value='Alex'>";
+?>
+
+<?php
+    echo "<input type=\"text\" name=\"name\" value=\"Alex\">";
+?>
+
