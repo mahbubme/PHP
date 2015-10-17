71c6aff0043f1b99845d50fd5075bbd6631389fd
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-17 09:18:32 +0000

    if/else statement

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+// if/else statement
+$number = 12;
+
+if ($number==10) {
+    echo 'Equal to ten.';
+}else if ($number==11) {
+    echo 'Equal to eleven.';
+}else {
+    echo 'Not equal.';
+}
+
+?>
