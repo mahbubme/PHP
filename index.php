5db2e3c0226ef83f9cfd201e77d60bfbe07d1250
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-18 14:07:38 +0000

    for loop

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+// For loop
+
+// Increment
+for ( $count=1 ; $count<=10 ; $count++ ) {
+    echo $count.'<br>';
+}
+
+// Decrement
+for ( $count=10 ; $count>=1 ; $count-- ){
+    echo $count.'<br>';
+}
+
+?>
