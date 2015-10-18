0457edb76e21b93f750a2c2ff10723390473df86
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-18 15:25:41 +0000

    functions with arguments

--- index.php
+++ index.php
@@ -11,10 +11,20 @@ MyName();
 // Functions with arguments
 $num1 = 5;
 $num2 = 30;
+
 function add($number1, $number2) {
-    echo $number1 + $number2;
+    echo $number1 + $number2.'<br>';
 }
 
+//
 add($num1, $num2);
+
+function displayDate($day, $date, $year) {
+    echo $day.' '.$date.' '.$year.'<br>';
+}
+
+displayDate('Sunday', 18, 2015);
+
+
 
 ?>
