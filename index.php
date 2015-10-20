f609be721cb163f2848037d6fbc873948c9b05ce
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-20 13:49:23 +0000

    string functions

--- index.php
+++ index.php
@@ -38,4 +38,28 @@ $string_slashes = htmlentities(addslashes($string));
 
 echo stripslashes($string_slashes);
 
+// Upper and lower case conversion
+$string = 'I Could Be Any Case.';
+$string_lower = strtolower($string);
+$string_upper = strtoupper($string);
+
+echo '<br>'.$string_lower;
+echo '<br>'.$string_upper;
+
+// Upper and lower case conversion in form data
+
+if (isset($_GET['user_name']) && !empty($_GET['user_name'])) {
+    $user_name = $_GET['user_name'];
+    $user_name_lc = strtolower($user_name);
+    
+    if ($user_name_lc=='alex') {
+        echo '<br>You are the best, '.$user_name;
+    }
+}
+
 ?>
+
+<form action="index.php" method="GET">
+    Name <input type="text" name="user_name"><br><br>
+    <input type="submit" value="Submit">
+</form>
