892e4318805d0ac48d4fa311a46897492fd73544
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-24 13:56:30 +0000

    checking if a file already exist

--- index.php
+++ index.php
@@ -13,4 +13,16 @@ if ($handle=opendir($directory.'/')) {
     }
 }
 
+// File Handling: Checking if a File Exists
+
+$filename = 'file2.txt';
+
+if (file_exists($filename)) {
+    echo 'File already exist.';
+}else {
+    $handle = fopen($filename, 'w');
+    fwrite($handle, 'I have nothing to write.');
+    fclose($handle);
+}
+
 ?>
