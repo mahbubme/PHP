019a481b17098e4efd25dd587b9ab82774518f46
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-10-24 14:12:22 +0000

    deleting files

--- index.php
+++ index.php
@@ -18,11 +18,26 @@ if ($handle=opendir($directory.'/')) {
 $filename = 'file2.txt';
 
 if (file_exists($filename)) {
-    echo 'File already exist.';
+    echo 'File already exist.<br>';
 }else {
     $handle = fopen($filename, 'w');
     fwrite($handle, 'I have nothing to write.');
     fclose($handle);
 }
+
+// File Handling: Deleting and Renaming Files
+
+// deleting files
+$filename = 'filetodelete.txt';
+
+if (@unlink($filename)) {
+    echo 'File '.$filename.' has been deleted.<br>';
+}else {
+    echo 'File cannot be deleted.<br>';
+}
+
+
+
+
 
 ?>
