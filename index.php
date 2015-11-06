36bdb12c05bc18ceaba64145c5c1336395517929
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-11-06 15:44:32 +0000

    remove isset function

--- index.php
+++ index.php
@@ -3,23 +3,20 @@
 // Scaling down images to thumbnails
 header('Content-Type: image/jpeg');
 
-if (isset($_GET['image'])) {
-    echo $image = $_GET['image'];
-    
-    $image_size = getimagesize($image);
-    $image_width = $image_size[0];
-    $image_height = $image_size[1];
-    
-    $new_size = ($image_width + $image_height)/($image_width*($image_height/45));
-    $new_width = $image_width * $new_size;
-    $new_height = $image_height * $new_size;
-    
-    $new_image = imagecreatetruecolor($new_width, $new_height);
-    $old_image = imagecreatefromjpeg($image);
-    
-    imagecopyresized($new_image, $old_image, 0, 0, 0, 0, $new_width, $new_height, $image_width, $image_height);
-    imagejpeg($new_image, $image.'.thumb.jpg', 100);
-    
-}
+$image = $_GET['image'];
 
+$image_size = getimagesize($image);
+$image_width = $image_size[0];
+$image_height = $image_size[1];
+
+$new_size = ($image_width + $image_height)/($image_width*($image_height/45));
+$new_width = $image_width * $new_size;
+$new_height = $image_height * $new_size;
+
+$new_image = imagecreatetruecolor($new_width, $new_height);
+$old_image = imagecreatefromjpeg($image);
+
+imagecopyresized($new_image, $old_image, 0, 0, 0, 0, $new_width, $new_height, $image_width, $image_height);
+imagejpeg($new_image, $image.'.thumb.jpg', 100);
+    
 ?>
