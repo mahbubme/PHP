07c73b0088cd64451696a6336eabb9879f5ac914
mahbubme <mahbuburrahmanmihir@gmail.com>
2015-11-09 12:58:24 +0000

    custom error messages

--- custom_exceptions.php
+++ custom_exceptions.php
@@ -5,21 +5,30 @@ $db_user = 'root';
 $db_pass = '';
 $db_name = 'a_database';
 
-class ServerException extends exception{};
-class DatabaseException extends exception{};
+class ServerException extends exception{
+    public function showSpecific() {
+        return 'Error thrown on line '.$this->getLine().' in '.$this->getFile();
+    }
+}
+
+class DatabaseException extends exception{
+    public function showSpecific() {
+        return 'Error thrown on line '.$this->getLine().' in '.$this->getFile();
+    }
+}
 
 try{
     if (!@mysql_connect($db_host, $db_user, $db_pass)) {
-        throw new ServerException('Could not connect to server.');
+        throw new ServerException();
     }else if (!@mysql_select_db($db_name)) {
-        throw new DatabaseException('Could not select database.');
+        throw new DatabaseException();
     }else {
         echo 'Connected.';
     }
 }catch (ServerException $ex) {
-    echo 'Error: '.$ex->getMessage();
+    echo $ex->showSpecific();
 }catch (DatabaseException $ex) {
-    echo 'Error: '.$ex->getMessage();
+    echo $ex->showSpecific();
 }
 
 
